8d56cb97dff0f8399df3687dd4b718fa1d507ce3
ramesh <ramesh786mullapudi@gmail.com>
2017-01-02 13:53:46 -0600

    Angular js SPA.

---
+++ conFusion/app/scripts/app.js
@@ -0,0 +1,65 @@
+'use strict';        
+angular.module('confusionApp',['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+    
+    $stateProvider
+    .state('app',{
+        url:'/',
+        views:{
+            'header':{
+                 templateUrl:'views/header.html'                
+            },
+            'content':{
+                template:'<h1>To be Completed</h1>',
+                controller:'IndexController'
+            },
+            'footer':{
+                templateUrl:'views/footer.html'
+            }
+        }
+    })
+    //route for aboutus page
+    .state('app.aboutus',{
+        url:'aboutus',
+        views:{
+            'content@':{
+                template:'<h1>To be completed</h1>',
+                controller:'AboutController'
+            }
+        }
+    })
+    //route for contactus page
+    .state('app.contactus',{
+        url:'contactus',
+        views:{
+            'content@':{
+                templateUrl:'views/contactus.html',
+                controller:'ContactController'
+            }
+        }
+    })
+    //route for menu page
+    .state('app.menu', {
+        url:'menu',
+        views:{
+            'content@':{
+                templateUrl:'views/menu.html',
+                controller:'MenuController'
+            }
+        }
+    })
+     //route for dishdetail page
+    .state('app.dishdetails', {
+        url:'menu/:id',
+        views:{
+            'content@':{
+                templateUrl:'views/dishdetail.html',
+                controller:'DishDetailController'
+            }
+        }
+    });
+    $urlRouterProvider.otherwise('/');
+})
+
+
+;
